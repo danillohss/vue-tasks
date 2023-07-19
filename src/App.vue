0f581e0a931ef8205aa1ce3b0ae367eb36de1143
Danillo Soares <danillohss@gmail.com>
2023-07-19 19:32:58 -0300

    Iniciando app tarefas

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
   <div id="app">
-    
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
         <h1 class="display-4">Requisições HTTP no Vue</h1>
-        <p class="lead">Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST.</p>
+        <p class="lead">
+          Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST.
+        </p>
       </div>
     </div>
 
     <div class="container">
-
       <TarefasLista />
-
     </div>
-
   </div>
 </template>
 
 <script>
-
-import TarefasLista from './components/TarefasLista.vue'
+import TarefasLista from "./components/TarefasLista.vue";
 
 export default {
-    components: {
-        TarefasLista
-    }
-}
+  components: {
+    TarefasLista,
+  },
+};
 </script>
--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -1,40 +1,45 @@
 <template>
-    <div>
+  <div>
+    <h1 class="font-weight-light">Lista de Tarefas</h1>
 
-        <h1 class="font-weight-light">Lista de Tarefas</h1>
+    <ul class="list-group" v-if="tarefas.length > 0">
+      <TarefasListaIten
+        v-for="tarefa in tarefas"
+        :key="tarefa.id"
+        :tarefa="tarefa"
+      />
+    </ul>
 
-        <ul class="list-group" v-if="tarefas.length > 0">
-            <TarefasListaIten
-                v-for="tarefa in tarefas"
-                :key="tarefa.id"
-                :tarefa="tarefa" />
-        </ul>
+    <p v-else>Nenhuma tarefa criada.</p>
 
-        <p v-else>Nenhuma tarefa criada.</p>
-
-        <TarefaSalvar />
-
-    </div>
+    <TarefaSalvar />
+  </div>
 </template>
 
 <script>
-
-import TarefaSalvar from './TarefaSalvar.vue'
-import TarefasListaIten from './TarefasListaIten.vue'
+import api from "../../service/api";
+import TarefaSalvar from "./TarefaSalvar.vue";
+import TarefasListaIten from "./TarefasListaIten.vue";
 
 export default {
-    components: {
-        TarefaSalvar,
-        TarefasListaIten
+  components: {
+    TarefaSalvar,
+    TarefasListaIten,
+  },
+  data() {
+    return {
+      tarefas: [],
+    };
+  },
+  methods: {
+    async getTarefas() {
+      await api.get("/tarefas").then((response) => {
+        this.tarefas = response.data;
+      });
     },
-    data() {
-        return {
-            tarefas: [
-                { id: 1, titulo: 'Aprender JavaScript', concluido: true },        
-                { id: 2, titulo: 'Aprender Vue', concluido: true },
-                { id: 3, titulo: 'Aprender Axios', concluido: false }
-            ]
-        }
-    }
-}
+  },
+  created() {
+    this.getTarefas();
+  },
+};
 </script>
